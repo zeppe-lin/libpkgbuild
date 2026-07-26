3a56f523c1f9ca8f2aa0e2abf8b179da4331b72b
Alexandr Savca <alexandr.savca89@gmail.com>
2026-07-26 12:16:03 +0000

    tests: follow corrected scdoc continuation markup

--- tests/check_manpage_contract.sh
+++ tests/check_manpage_contract.sh
@@ -7,7 +7,7 @@ page="$srcdir/man/pkgbuild_plan_adapter.3.scd"
 
 grep -F '*pkgbuild::plan_adapter::artifact_projection*++' "$page" >/dev/null
 grep -F '*pkgbuild::plan_adapter::project_artifact(* \' "$page" >/dev/null
-grep -F '    *pkgbuild::BuildReceipt build,*++' "$page" >/dev/null
+grep -F '    *pkgbuild::BuildReceipt build,* \' "$page" >/dev/null
 grep -F '    *const pkgimage::archive_backend& archives*);' "$page" >/dev/null
 
 if grep -E '^[0-9]+\. ' "$srcdir"/man/*.scd >/dev/null; then
